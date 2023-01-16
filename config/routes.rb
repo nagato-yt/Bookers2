4086e455bd0f946a82e4e5b5376893f9824b5665
nagato-yt <119833402+nagato-yt@users.noreply.github.com>
2023-01-16 11:37:03 +0000

    update トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/show'
-  get 'books/new'
-  get 'books/show'
-  get 'books/index'
-  get 'books/edit'
-  get 'books/destroy'
-  get 'books/create'
-  get 'homes/top'
+  
   devise_for :users
+  root to: 'homes#top'
+  resources :books
+  resources :users, only:[:show,:edit]
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
